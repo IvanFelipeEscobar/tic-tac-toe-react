7ad00934d6fa1078848e9afa8e3cc1d62540251a
Ivan Escobar <ivnescbr1989@gmail.com>
2023-04-26 14:34:51 -0400

    pass square placement value as a state

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,12 +1,13 @@
-import {useState} from 'react'
-const Square = () => {
-    const [value, setValue] = useState(null)
-    const handleClick = () => {
-        console.log(`click`)
-        setValue(`x`)
-    }
-    return (
-        <button className="square" onClick={handleClick}>{value}</button>
-    )
+// import {useState} from 'react'
+const Square = ({value}) => {
+    // const [value, setValue] = useState(null)
+    // const handleClick = () => {
+    //     console.log(`click`)
+    //     setValue(`x`)
+    // }
+    // return (
+    //     <button className="square" onClick={handleClick}>{value}</button>
+    // )
+    return <button className='square'>{value}</button>
 }
 export default Square
