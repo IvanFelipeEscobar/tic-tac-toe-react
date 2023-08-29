3d8a0bae82f5c8ca4498f0ab5a4c866f24cc8a45
Ivan Escobar <ivnescbr1989@gmail.com>
2023-08-29 18:53:09 -0400

    clixk => x

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,13 +1,17 @@
 // import {useState} from 'react'
-const Square = ({value}) => {
-    // const [value, setValue] = useState(null)
-    // const handleClick = () => {
-    //     console.log(`click`)
-    //     setValue(`x`)
-    // }
-    // return (
-    //     <button className="square" onClick={handleClick}>{value}</button>
-    // )
-    return <button className='square'>{value}</button>
-}
-export default Square
+const Square = ({value, onSquareClick}) => {
+//   const [value, setValue] = useState(null)
+//   const handleClick = () => {
+//       console.log(`click`)
+//       setValue(`x`)
+//   }
+  
+  return (
+    <button className="square" 
+    onClick={onSquareClick}
+    >
+      {value}
+    </button>
+  );
+};
+export default Square;
